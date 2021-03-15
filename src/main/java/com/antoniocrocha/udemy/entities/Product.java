1c6dfd5867a998e3718ee20bab17be0384d2c718
antoniocarlos-bc <antoniocarlos_bc@outlook.com>
2021-03-14 22:12:46 -0300

    Many-to-Many association with joinTable

--- src/main/java/com/antoniocrocha/udemy/entities/Product.java
+++ src/main/java/com/antoniocrocha/udemy/entities/Product.java
@@ -8,8 +8,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 
 @Entity
@@ -25,8 +27,11 @@ public class Product implements Serializable{
 	private Double price;
 	private String imgUrl;
 	
-	@Transient
-	private Set<Product> categories = new HashSet<>();
+	@ManyToMany
+	@JoinTable(name = "tb_product_category",
+	joinColumns = @JoinColumn(name = "product_id"),
+	inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private Set<Category> categories = new HashSet<>();
 	
 	
 	
@@ -106,7 +111,7 @@ public class Product implements Serializable{
 
 
 
-	public Set<Product> getCategories() {
+	public Set<Category> getCategories() {
 		return categories;
 	}
 
